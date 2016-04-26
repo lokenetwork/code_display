3171a443f0439ccae0335b89cfd42643cf00e9f6
loken <lokenetwork@users.noreply.github.com>
2016-04-26 22:52:32 +0800

    为什么用&main_number_2_point不成功

--- main.c
+++ main.c
@@ -2,28 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void change_number_1(int * number){
-    *number = 10;
-}
 
-void change_number_2(int ** number){
-    *number = 10;
+void change_number_2(int ** number) {
+    *number = 40;
 }
 
 int main() {
-    int main_number_1 = 5;
-    int * main_number_1_point = &main_number_1;
-
-    change_number_1(&main_number_1_point);
-    printf("%d\n",main_number_1);
-
     int main_number_2 = 45;
+    printf("%d\n", main_number_2);
     int * main_number_2_point = &main_number_2;
-
-    change_number_2(&main_number_2);
-    printf("%d\n",main_number_2);
-
-
-
+    change_number_2(main_number_2_point);
+    //为什么用&main_number_2_point不成功
+    //change_number_2(&main_number_2_point);
+    printf("%d\n", main_number_2);
     return (0);
 }
