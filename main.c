3fffab9e57371ad15fe3c6f08c49b1ea17e408d8
loken <lokenetwork@users.noreply.github.com>
2016-04-27 22:22:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(void)
 
    /*
     * 如何不用pthread_join来通信，因为我子进程可能会递归多N多子进程，直接一个pthread_join不行。好像管道可以，
-    * 我就想，子进程通知main，不管哪个子进程通知都行。有什么方法？
+    * 我就想，子进程通知main，不管哪个子进程通知都行，就是main要等待所有子进程执行完。有什么方法？
     */
     //pthread_join(thread_id_1,NULL);
     return (0);
