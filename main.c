8061778e18f5805432ba8c5a886ea25adbed493a
loken <lokenetwork@users.noreply.github.com>
2016-04-28 17:22:43 +0800

    __sync_fetch_and_add 原子操作。

--- main.c
+++ main.c
@@ -6,18 +6,32 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <memory.h>
+
+pthread_mutex_t count_lock = PTHREAD_MUTEX_INITIALIZER;
 
 int my_count = 0;
 
+int add_type=2;
+
+
 void thread() {
-    for (int i = 0; i < 100; i++) {
-        //我开了5个线程模拟并发写，怎么没有冲突，每次都是500的结果
-        my_count++;
+    for (int i = 0; i < 100000; i++) {
+        if( add_type == 1 ){
+            //printf("线程锁相加 starting\n");
+            pthread_mutex_lock(&count_lock);
+            my_count++;
+            pthread_mutex_unlock(&count_lock);
+        }else {
+            //printf("原子相加 starting\n");
+            __sync_fetch_and_add( &my_count, 1 );
+
+        }
     }
 }
 
-
 int main(void) {
+
     int ret;
     pthread_t thread_id_1, thread_id_2, thread_id_3,thread_id_4,thread_id_5;
     ret = pthread_create(&thread_id_1, NULL, (void *) thread, NULL);
