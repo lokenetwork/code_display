dbf700b64a897379b9119f0e386cf5d9c14518da
loken <lokenetwork@users.noreply.github.com>
2016-04-28 23:03:53 +0800

    //原来free掉结构体指针并不会free掉结构体里面的char *指针元素

--- main.c
+++ main.c
@@ -5,62 +5,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <memory.h>
-
-pthread_mutex_t count_lock = PTHREAD_MUTEX_INITIALIZER;
-
-int my_count = 0;
-
-int add_type=2;
-
-
-void thread() {
-    for (int i = 0; i < 100000; i++) {
-        if( add_type == 1 ){
-            //printf("线程锁相加 starting\n");
-            pthread_mutex_lock(&count_lock);
-            my_count++;
-            pthread_mutex_unlock(&count_lock);
-        }else {
-            //printf("原子相加 starting\n");
-            __sync_fetch_and_add( &my_count, 1 );
-
-        }
-    }
+//父线程传参給子线程的结构体
+struct TREE_CUT_WORDS_DATA {
+    char *cut_word_result;
+};
+void thread(struct TREE_CUT_WORDS_DATA *parent_cut_words_data) {
+    //原来free掉结构体指针并不会free掉结构体里面的char *指针元素
+    //free(parent_cut_words_data->cut_word_result);
+    free(parent_cut_words_data);
 }
 
 int main(void) {
 
-    int ret;
-    pthread_t thread_id_1, thread_id_2, thread_id_3,thread_id_4,thread_id_5;
-    ret = pthread_create(&thread_id_1, NULL, (void *) thread, NULL);
-    if (ret != 0) {
-        printf("pthread_create error.\n");
-    }
-    ret = pthread_create(&thread_id_2, NULL, (void *) thread, NULL);
-    if (ret != 0) {
-        printf("pthread_create error.\n");
-    }
-    ret = pthread_create(&thread_id_3, NULL, (void *) thread, NULL);
-    if (ret != 0) {
-        printf("pthread_create error.\n");
-    }
-    ret = pthread_create(&thread_id_4, NULL, (void *) thread, NULL);
-    if (ret != 0) {
-        printf("pthread_create error.\n");
-    }
-    ret = pthread_create(&thread_id_5, NULL, (void *) thread, NULL);
-    if (ret != 0) {
-        printf("pthread_create error.\n");
-    }
-    pthread_join(thread_id_1,NULL);
-    pthread_join(thread_id_2,NULL);
-    pthread_join(thread_id_3,NULL);
-    pthread_join(thread_id_4,NULL);
-    pthread_join(thread_id_5,NULL);
 
-    printf("my account is %d\n", my_count);
-    printf("This is the main end.\n");
+    struct TREE_CUT_WORDS_DATA * parent_cut_words_data = (struct TREE_CUT_WORDS_DATA *) malloc(sizeof(struct TREE_CUT_WORDS_DATA));
+    memset(parent_cut_words_data,0,sizeof(struct TREE_CUT_WORDS_DATA));
+    parent_cut_words_data->cut_word_result = (char *) malloc(10);
+    memset(parent_cut_words_data->cut_word_result,0,10);
+    char * name = (char *) malloc(10);
+    memset(name,0,10);
+    strcpy(name,"loken");
+    strcpy(parent_cut_words_data->cut_word_result,name);
+    char * back_name = parent_cut_words_data->cut_word_result;
+    printf("back_name point is %p\n",back_name);
+    printf("parent_cut_words_data->cut_word_result point is %p\n",parent_cut_words_data->cut_word_result);
+
+
+    pthread_t thread_id_1;
+    int ret = pthread_create(&thread_id_1, NULL, (void *) thread, parent_cut_words_data);
+    if (ret != 0) {
+        printf("pthread_create error.\n");
+    }
+    sleep(2);
+
+    printf("back_name is %s\n",back_name);
+
+    printf("This is the main .\n");
     return (0);
 }
